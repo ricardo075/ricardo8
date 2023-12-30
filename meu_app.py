f6f393dcb0f513135f11e13e37f1efa178e339dc
ricardo075 <155269880+ricardo075@users.noreply.github.com>
2023-12-31 01:32:00 +0200

    Add files via upload

---
+++ meu_app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+st.set_page_config(page_title='SITE MOZ')
+st.title('Aqui encontras os melhores pratos moçambicanos e claro um pouco de tudo.')
+st.write('PARA O CANAL DO YOUTUBE [clique aqui](https://youtu.be/nJWdqldYxc8?si=bVHaPRDRk5XYRbjT)')
+st.write('Para hoje iremos ver a receita de gulabos ')
+st.subheader('...................................GULABOS.................................')
+st.write('IGREDIENTES PARA PREPARAÇAO DA MASSA:')
+with st.container():
+    st.write('#7 OVOS')
+    st.write('#500 g de manteiga')
+    st.write('#1 lata de leite condensado')
+    st.write('#1 colher de cha de raspa de limao')
+    st.write('#2 colheres de cha de custarde')
+    st.write('#2 colheres de royal')
+    st.write('#1 pacote de coco ralado')
+    st.write('#Trigo ate dar o ponto')
+with st.container():
+    st.write('---')
+    st.write('MODO DE PREPARO')
